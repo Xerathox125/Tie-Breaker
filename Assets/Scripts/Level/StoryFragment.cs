742cb6e56a19198ebbe87ba61af55d17f1334fa3
Xerathox <joaquinaguirrep125@gmail.com>
2026-08-21 14:55:31 -0500

    .

--- Assets/Scripts/Level/StoryFragment.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "FragmentoHistoria", menuName = "Juego/Fragmento de Historia")]
-public class StoryFragment : ScriptableObject
-{
-    [TextArea(3, 10)]
-    public string textoLore;
-    public Sprite imagenFondo;
-}
